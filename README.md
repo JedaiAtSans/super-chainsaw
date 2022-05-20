2075519c494e3fbcb102eb8531224b8b8472ad08
Jedai Saboteur <jsaboteur@sans.org>
2022-05-20 15:13:11 -0700

    Added prometheus metrics endpoint

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+docker build https://github.com/JedaiAtSans/super-chainsaw.git#1.0.0:docker
+
+docker build https://github.com/JedaiAtSans/super-chainsaw.git#1.0.0 -t jsaboteur/super-chainsaw:1.0.0
+```
--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 'use strict';
 
 const express = require('express');
-
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+const register = client.register;
+collectDefaultMetrics()
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/metrics', async (req, res) => {
+	try {
+		res.set('Content-Type', register.contentType);
+		res.end(await register.metrics());
+	} catch (ex) {
+		res.status(500).end(ex);
+	}
 });
 
 app.listen(PORT, HOST);
